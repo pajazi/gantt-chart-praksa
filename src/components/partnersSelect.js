ef3cb041a60b987902eb403e806e25bbd765f2f1
Teodora Cvetkovic <teodra.cvetkovic@elfak.rs>
2024-11-12 19:13:35 +0100

    testing

--- src/components/partnersSelect.js
+++ src/components/partnersSelect.js
@@ -24,7 +24,7 @@ function getStyles(name, personName, theme) {
             : theme.typography.fontWeightRegular,
     }
 }
-
+//testing
 const PartnersSelect = () => {
     const { data: partners } = useGetPartnersQuery()
 
